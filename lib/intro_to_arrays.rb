d594e984641ab602e565116e65cfe4c8b97d1417
Angela Mumba <malaika.mumba@gmail.com>
2019-05-06 20:39:29 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+def instantiate_new_array
+  array = []
+end 
+
+def array_with_two_elements
+  array = [1,2]
+end 
+
+def first_element(array)
+  array = [1,"Welcome to New York",3,4,5,6]
+  array[1]
+end 
+
+def third_element(array)
+  array = [1,"Welcome to New York",3,4,5,6]
+  array[3]
+end 
+
+def last_element(array)
+  array = [1,"Welcome to New York",3,4,5,6]
+  array[-1]
+end 
+
