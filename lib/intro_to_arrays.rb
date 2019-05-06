94593acb0fd6c5a187eeb4fdee927ee6c72312cd
Angela Mumba <malaika.mumba@gmail.com>
2019-05-06 20:45:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -26,3 +26,8 @@ def first_element_with_array_methods(array)
   array.first 
 end 
 
+def last_element_with_array_methods(array)
+  array = ["Thailand","Welcome to New York",3,4,5,"Out of The Woods"]
+  array.last 
+end 
+
